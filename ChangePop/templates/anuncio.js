7f33c7e2e451549c8ec245ae78f7e0dcd603d7f3
ferleci <ferlandamarcen@hotmail.com>
2019-05-24 01:30:01 +0200

    Modificado añadir anuncio

--- ChangePop/templates/anuncio.js
+++ ChangePop/templates/anuncio.js
@@ -1,11 +1,13 @@
 var checkbox=[];
-
+var imagen=0;
+var categories=[];
 function validar_campos(names) {
     var correct = 0;
 
     for(var i=0;i<checkbox.length;i++){
         if(document.getElementById(checkbox[i]).checked){
             correct=1;
+            categories.push(document.getElementById(checkbox[i]).id);
         }
     }
 
@@ -15,11 +17,30 @@ function validar_campos(names) {
             correct = 0;
         }
     }
-    if (correct==0){
+    if (correct==0 || imagen == 0){
         alert("Hay campos mal rellenados");
+    }else{
+        //falta incluir bid_date
+        var descript=document.getElementById("blog_body").value;
+        var price=document.getElementById("precio").value;
+        var title=document.getElementById("nombreProducto").value;
+        var bid_date="";
+        var main_img=document.getElementById("mostrar").src;
+        var place=document.getElementById("lugarRecogida").value;
+        var data = JSON.stringify({"descript": descript, "price": price, "categories": categories, "title": title, "bid_date": bid_date, "main_img": main_img, "photo_urls": main_img, "place": place});
+        console.log(data);
     }
 }
 
 function add_checkbox(nuevo){
     checkbox.push(nuevo);
 }
+
+function change_imagen(){
+    imagen=1;
+}
+
+function make_json(){
+    return "{"
+}
+
