375a9d45ab3844c3ac6721a9dcd98bda5f7ba06f
bolumclol <bolu1997@gmail.com>
2019-05-24 22:28:58 +0200

    prueba

--- ChangePop/templates/httpGet.js
+++ ChangePop/templates/httpGet.js
@@ -19,7 +19,11 @@
       }
       function post(url,params,respuesta){
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = respuesta();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            respuesta();
+          }
+        };  
         xhttp.open("POST", url, true);
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.send(JSON.stringify(params));
