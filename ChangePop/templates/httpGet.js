0f598c8807821bcb83a3d815730215236fad6f80
ferleci <ferlandamarcen@hotmail.com>
2019-05-28 13:20:46 +0200

    Index modificado

--- ChangePop/templates/httpGet.js
+++ ChangePop/templates/httpGet.js
@@ -17,11 +17,13 @@
           if (!results[2]) return '';
           return decodeURIComponent(results[2].replace(/\+/g, ' '));
       }
-      function post(url,params,respuesta){
+      function post(url,params,respuesta,respuesta2){
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
             respuesta();
+          }else if(this.status == 400){
+            respuesta2();
           }
         };  
         xhttp.open("POST", url, true);
