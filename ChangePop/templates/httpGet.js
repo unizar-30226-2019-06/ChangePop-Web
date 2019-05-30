97bc3c86661e4b87e846b76f69b264353f0c39f7
ferleci <ferlandamarcen@hotmail.com>
2019-05-30 10:26:06 +0200

    Nuevo post y get

--- ChangePop/templates/httpGet.js
+++ ChangePop/templates/httpGet.js
@@ -60,3 +60,51 @@
         xhttp.send(JSON.stringify(params));
       }
 
+      function getCORS(theUrl,respuesta){
+        $.ajax({
+      type: 'get',
+      url:   theUrl,      
+      crossDomain: true,
+      xhrFields: {
+          withCredentials: true
+       },
+      headers:{    
+         'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' 
+      },
+      success: function (data) {
+         respuesta(data);
+       
+      }
+
+  });
+
+       }
+
+function postLOL(url,params,respuesta,respuesta2) {
+     $.ajax({
+      type: 'post',
+      url: url,
+      crossDomain: true,
+      xhrFields: {
+      withCredentials: true
+    },
+      headers:{    
+         'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*' ,
+        'Content-Type': 'application/json'
+        },
+      data: JSON.stringify(params),          
+      success: function (data) {
+         respuesta(data);
+        
+      },
+      error: function(data){
+        respuesta2();
+      }
+
+
+  });
+    }
+
